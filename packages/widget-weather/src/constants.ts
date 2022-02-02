3786cb8ecdc61ce3b65b32918fbc6c414ecc8ba8
Christian Bromann <git@bromann.dev>
2022-02-02 14:34:51 +0100

    refactor state management for weather widget

---
+++ packages/widget-weather/src/constants.ts
@@ -0,0 +1,11 @@
+export const SCALE_OPTIONS = [
+  { name: "Fahrenheit", value: "fahrenheit" },
+  { name: "Celsius", value: "celsius" },
+] as const;
+
+export const DEFAULT_CONFIGURATION = {
+  city: undefined,
+  scale: SCALE_OPTIONS[0].name
+};
+
+export const DEFAULT_STATE = {};
