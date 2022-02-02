c5a8fdcd2ed13570ae4e7313c3fe3fb0150ac950
Christian Bromann <git@bromann.dev>
2022-02-02 23:07:37 +0100

    refactor state management for notes widget

---
+++ packages/widget-notes/src/extension.ts
@@ -0,0 +1,33 @@
+import vscode from 'vscode';
+
+import ExtensionManager from '@vscode-marquee/utils/build/extensionManager';
+
+import { DEFAULT_STATE } from './constants';
+import type { State } from './types';
+
+const STATE_KEY = 'widgets.notes';
+
+class TodoExtensionManager extends ExtensionManager<State, {}> {
+  constructor (
+    context: vscode.ExtensionContext,
+    channel: vscode.OutputChannel
+  ) {
+    super(context, channel, STATE_KEY, {}, DEFAULT_STATE);
+  }
+}
+
+export function activate (
+  context: vscode.ExtensionContext,
+  channel: vscode.OutputChannel
+) {
+  const stateManager = new TodoExtensionManager(context, channel);
+
+  return {
+    marquee: {
+      disposable: stateManager,
+      defaultState: stateManager.state,
+      defaultConfiguration: stateManager.configuration,
+      setup: stateManager.setBroadcaster.bind(stateManager)
+    }
+  };
+}
