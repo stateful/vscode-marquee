7d20ae9954f50652614c48aeb39c2bec56433cce
Christian Bromann <git@bromann.dev>
2022-02-02 23:07:37 +0100

    refactor state management for notes widget

--- packages/widget-notes/src/Context.tsx
+++ packages/widget-notes/src/Context.tsx
@@ -1,41 +1,24 @@
 import React, { createContext, useState, useEffect } from "react";
-import { store, createConsumer, getEventListener, MarqueeEvents } from "@vscode-marquee/utils";
+import { connect, getEventListener, MarqueeEvents, MarqueeWindow } from "@vscode-marquee/utils";
 
-import { IContext, Note } from './types';
 import AddDialog from "./dialogs/AddDialog";
 import EditDialog from "./dialogs/EditDialog";
+import { DEFAULT_STATE } from "./constants";
+import type { State, Context, Note } from './types';
 
-const NoteContext = createContext<IContext>({} as IContext);
+declare const window: MarqueeWindow;
+const NoteContext = createContext<Context>({} as Context);
 
 const NoteProvider = ({ children }: { children: React.ReactElement }) => {
   const eventListener = getEventListener<MarqueeEvents>();
-  const noteStore = store("notes", false);
+  const widgetState = getEventListener<State>('@vscode-marquee/notes-widget');
+  const providerValues = connect<State>(DEFAULT_STATE, widgetState);
 
-  const [notes, setNotes] = useState([] as Note[]);
-  const [activeWorkspaceId, setActiveWorkspaceId] = useState(null as (string | null));
-  const [noteFilter, setNoteFilter] = useState("");
-  const [noteSelected, setNoteSelected] = useState("");
-  const [noteSplitter, setNoteSplitter] = useState(80);
   const [showAddDialog, setShowAddDialog] = useState(false);
   const [showEditDialog, setShowEditDialog] = useState<string | undefined>();
 
-  const _updateNoteFilter = (noteFilter: string) => {
-    setNoteFilter(noteFilter);
-    noteStore.set("noteFilter", noteFilter);
-  };
-
-  const _updateNoteSelected = (noteSelected: string) => {
-    setNoteSelected(noteSelected);
-    noteStore.set("noteSelected", noteSelected);
-  };
-
-  const _updateNoteSplitter = (percentage: number) => {
-    setNoteSplitter(percentage);
-    noteStore.set("noteSplitter", percentage);
-  };
-
-  const _addNote = (note: Partial<Note>, cb?: (id: string) => void) => {
-    const globalNotes = noteStore.get("notes") || [];
+  const _addNote = (note: Pick<Note, 'title' | 'body' | 'text'>, isWorkspaceTodo: boolean): string => {
+    const globalNotes = providerValues.notes;
     const id = [...Array(8)].map(() => Math.random().toString(36)[2]).join('');
 
     const newNote = Object.assign({}, note, {
@@ -43,62 +26,41 @@ const NoteProvider = ({ children }: { children: React.ReactElement }) => {
       archived: false,
       createdAt: new Date().getTime(),
       origin: null,
-      workspaceId: activeWorkspaceId,
+      workspaceId: isWorkspaceTodo
+        ? window.activeWorkspace?.id || null
+        : null,
     });
 
     globalNotes.unshift(newNote);
-    setNotes(globalNotes);
-    noteStore.set("notes", globalNotes);
-
-    if (cb) {
-      cb(id);
-    }
+    providerValues.setNotes(globalNotes);
+    return id;
   };
 
   const _removeNote = (id: string) => {
-    const globalNotes: Note[] = noteStore.get("notes") || [];
+    const globalNotes = providerValues.notes;
     const index = globalNotes.findIndex((note) => note.id === id);
-    globalNotes.splice(index, 1);
 
-    setNotes(globalNotes);
-    noteStore.set("notes", globalNotes);
+    if (index < 0) {
+      return console.error(`Couldn't find note with id "${id}"`);
+    }
+
+    globalNotes.splice(index, 1);
+    providerValues.setNotes(globalNotes);
   };
 
   const _updateNote = (note: Note) => {
-    const globalNotes: Note[] = noteStore.get("notes") || [];
+    const globalNotes = providerValues.notes;
     const index = globalNotes.findIndex((entry) => entry.id === note.id);
-    globalNotes[index] = note;
-    setNotes(globalNotes);
-    noteStore.set("notes", globalNotes);
-  };
 
-  const _setNotes = (notes: Note[]) => {
-    setNotes(notes);
-    noteStore.set("notes", notes);
-  };
-
-  const handler = () => {
-    const globalNotes = noteStore.get("notes") || [];
-    if (JSON.stringify(globalNotes) !== JSON.stringify(notes)) {
-      setNotes(globalNotes);
-    } else if (notes.length === 0) {
-      setNotes(globalNotes);
+    if (index < 0) {
+      return console.error(`Couldn't find note with id "${note.id}"`);
     }
 
-    setNoteFilter(noteStore.get("noteFilter") || "");
-    setNoteSelected(noteStore.get("noteSelected") || 0);
-    setNoteSplitter(noteStore.get("noteSplitter") || 80);
+    globalNotes[index] = note;
+    providerValues.setNotes(globalNotes);
   };
 
   useEffect(() => {
-    noteStore.subscribe(handler as any);
-    handler();
-    createConsumer("activeWorkspace").subscribe((aws) => {
-      if (aws && aws.id) {
-        setActiveWorkspaceId(aws.id);
-      }
-    });
-
     eventListener.on('openAddNoteDialog', setShowAddDialog);
     eventListener.on('openEditNoteDialog', setShowEditDialog);
   }, []);
@@ -106,17 +68,10 @@ const NoteProvider = ({ children }: { children: React.ReactElement }) => {
   return (
     <NoteContext.Provider
       value={{
-        notes,
-        noteFilter,
-        noteSelected,
-        noteSplitter,
+        ...providerValues,
         _addNote,
         _removeNote,
         _updateNote,
-        _setNotes,
-        _updateNoteFilter,
-        _updateNoteSelected,
-        _updateNoteSplitter,
 
         showAddDialog,
         setShowAddDialog,
@@ -127,8 +82,8 @@ const NoteProvider = ({ children }: { children: React.ReactElement }) => {
       {showAddDialog && (
         <AddDialog close={() => setShowAddDialog(false)} />
       )}
-      { showEditDialog && notes.find((n) => n.id === showEditDialog) && (
-        <EditDialog note={notes.find((n) => n.id === showEditDialog)!} close={() => setShowEditDialog(undefined) } />
+      { showEditDialog && providerValues.notes.find((n) => n.id === showEditDialog) && (
+        <EditDialog note={providerValues.notes.find((n) => n.id === showEditDialog)!} close={() => setShowEditDialog(undefined) } />
       )}
       {children}
     </NoteContext.Provider>
