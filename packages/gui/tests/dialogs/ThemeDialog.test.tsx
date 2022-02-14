a7612b2dce54ebe53959c55b02840d47797252c3
Christian Bromann <git@bromann.dev>
2022-02-14 22:21:25 +0100

    allow changing background through click on image (#20)

--- packages/gui/tests/dialogs/ThemeDialog.test.tsx
+++ packages/gui/tests/dialogs/ThemeDialog.test.tsx
@@ -3,18 +3,22 @@ import userEvent from '@testing-library/user-event';
 import { render } from '@testing-library/react';
 
 import ThemeDialog from "../../src/dialogs/ThemeDialog";
-import { GlobalProvider } from '@vscode-marquee/utils';
+// @ts-expect-error
+import { GlobalProvider, setBackground } from '@vscode-marquee/utils';
 
 jest.mock('../../../utils/src/contexts/Global');
 jest.mock('../../src/utils/backgrounds', () => jest.fn((bg) => bg));
 
 test('should render component properly', () => {
   const close = jest.fn();
-  const { getByText } = render(
+  const { getByText, container } = render(
     <GlobalProvider>
       <ThemeDialog close={close} />
     </GlobalProvider>
   );
   userEvent.click(getByText('Close'));
   expect(close).toBeCalledTimes(1);
+
+  userEvent.click(container.querySelectorAll('.MuiTypography-body2')[7]);
+  expect(setBackground).toBeCalledWith('8');
 });
