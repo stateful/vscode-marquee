d5f631b91ad8332214bbab073bea6d6a3075e5a9
Christian Bromann <git@bromann.dev>
2022-02-25 14:58:02 +0100

    fix date test

--- packages/widget-weather/tests/utils.test.ts
+++ packages/widget-weather/tests/utils.test.ts
@@ -1,6 +1,6 @@
 import { formatAMPM } from '../src/utils';
 
 test('formatAMPM', () => {
-  expect(formatAMPM(new Date(1645789607201))).toBe('12:46 PM');
-  expect(formatAMPM(new Date(1645782607201))).toBe('10:50 AM');
+  expect(formatAMPM(new Date('Fri Feb 25 2022 14:56:41 GMT+0100 (Central European Standard Time)'))).toBe('2:56 PM');
+  expect(formatAMPM(new Date('Fri Feb 25 2022 10:11:41 GMT+0100 (Central European Standard Time)'))).toBe('10:11 AM');
 });
