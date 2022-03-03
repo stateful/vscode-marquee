c0b56270cecd767d544f1c837b1fa585d2b5f0bd
Christian Bromann <git@bromann.dev>
2022-03-03 18:39:55 +0100

    fix prerelease version

--- .github/scripts/updateEdgeVersion.js
+++ .github/scripts/updateEdgeVersion.js
@@ -12,7 +12,14 @@ const pkgPath = path.join(__dirname, '..', '..', 'package.json');
 const pkg = JSON.parse(fs.readFileSync(pkgPath).toString());
 
 const newVersion = pkg.version.split('.').slice(0, 2)
-newVersion.push(Date.now());
+
+/**
+ * VSCode Marketplace version requirements:
+ * It must be one to four numbers in the range 0 to 2147483647,
+ * with each number seperated by a period. It must contain at least one non-zero number.
+ */
+const prereleaseDate = Math.floor(Date.now() / 1000);
+newVersion.push(prereleaseDate);
 pkg.version = `${newVersion.join('.')}`;
 
 console.log(`Update package.json with Edge version:\n\n${JSON.stringify(pkg, null, 2)}`);
