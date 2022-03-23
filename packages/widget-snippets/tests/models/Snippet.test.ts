02bcc2f7ccc96231238b34005cfbd81229c40f92
Christian Bromann <git@bromann.dev>
2022-03-23 21:29:36 +0100

    tweak

--- packages/widget-snippets/tests/models/Snippet.test.ts
+++ packages/widget-snippets/tests/models/Snippet.test.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+import pathImport from 'path';
 import Snippet from '../../src/models/Snippet';
 
 describe('Snippet', () => {
@@ -6,7 +6,7 @@ describe('Snippet', () => {
     const s = new Snippet();
     expect(s.workspaceId).toBe(null);
     expect(s.title).toBe('New Snippet');
-    expect(s.path).toBe(path.sep + path.join('39d2f858-3522-4053-beb8-b75a1defd1d2', 'New Snippet'));
+    expect(s.path).toBe(pathImport.sep + pathImport.join('39d2f858-3522-4053-beb8-b75a1defd1d2', 'New Snippet'));
   });
 
   it('can be created from snippet object', () => {
