f8ef98a8a26ccdcc6473f705139a9fd37b85f55f
Christian Bromann <git@bromann.dev>
2022-06-02 21:21:01 +0200

    update deps

--- test/wdio.conf.ts
+++ test/wdio.conf.ts
@@ -80,18 +80,14 @@ export const config: Options.Testrunner = {
   // https://saucelabs.com/platform/platform-configurator
   //
   capabilities: [{
-
-    // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-    // grid with only 5 firefox instances available you can make sure that not more than
-    // 5 instances get started at a time.
-    maxInstances: 5,
-    //
-    browserName: 'chrome',
-    acceptInsecureCerts: true
-    // If outputDir is provided WebdriverIO can capture driver session logs
-    // it is possible to configure which logTypes to include/exclude.
-    // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-    // excludeDriverLogs: ['bugreport', 'server'],
+    browserName: 'vscode',
+    browserVersion: 'stable',
+    // @ts-expect-error these caps are not typed in WebdriverIO
+    'wdio:vscodeOptions': {
+      verboseLogging: false,
+      extensionPath: path.join(__dirname, '..'),
+      workspacePath: path.join(__dirname, '..')
+    }
   }],
   //
   // ===================
@@ -140,13 +136,7 @@ export const config: Options.Testrunner = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: [
-    ['vscode', {
-      verboseLogging: true,
-      extensionPath: path.join(__dirname, '..'),
-      workspacePath: path.join(__dirname, '..')
-    }]
-  ],
+  services: ['vscode'],
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
