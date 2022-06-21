ab8b6a9f56e4959e31a4299bd784efbf45cc27e2
Christian Bromann <git@bromann.dev>
2022-06-21 19:31:13 +0200

    remove console.log

--- packages/widget/src/WidgetWrapper.tsx
+++ packages/widget/src/WidgetWrapper.tsx
@@ -58,8 +58,6 @@ export default (Widget: any, name?: string) => React.memo(React.forwardRef((prop
     fullscreenMode
   }
 
-  console.log('RENDER!!!!', props.name)
-
   return (
     <ErrorBoundary>
       <WidgetWrapper innerref={ref} name={name!} dragHandle={props.children} {...props}>
