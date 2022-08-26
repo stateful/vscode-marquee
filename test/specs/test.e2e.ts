40b87822aa964ff1de61d37065c5a40dc0ccf0fe
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2022-08-26 12:51:40 -0400

    Fix test: HN Best <> Newest

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -82,7 +82,7 @@ describe('Marquee', () => {
         const firstArticleText = await firstArticle.getText()
         expect(typeof firstArticleText).toBe('string')
 
-        await newsWidget.switchChannel('HN Best')
+        await newsWidget.switchChannel('HN Newest')
 
         const newFirstArticle = await newsWidget.getArticle(0)
         const newFirstArticleText = await newFirstArticle.getText()
